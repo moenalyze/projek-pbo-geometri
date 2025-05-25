b019797ae9ccff1af9637a3f21cde5c16f8ab0f4
Khatama <152944169+moenalyze@users.noreply.github.com>
2025-05-25 22:10:57 +0700

    edit belahketupat

--- src/projekpbo/bangunDatar/BelahKetupat.java
+++ src/projekpbo/bangunDatar/BelahKetupat.java
@@ -6,26 +6,39 @@ public class BelahKetupat extends Geometri2D {
     // Atribut khusus untuk belah ketupat
     protected double diagonal1;
     protected double diagonal2;
+    protected double sisi;
+    private double luas;
+    private double keliling;
 
     // Constructor untuk BelahKetupat
     public BelahKetupat(double diagonal1, double diagonal2) {
         this.diagonal1 = diagonal1;
         this.diagonal2 = diagonal2;
+        // Sisi miring dihitung dari √((d1/2)² + (d2/2)²)
+        sisi = Math.sqrt(Math.pow((1.0/2.0 * diagonal1), 2) + Math.pow(1.0/2.0 * diagonal2, 2));
     }
    
     // Implementasi metode hitungLuas() untuk belah ketupat
     @Override
     public double hitungLuas() {
         // Rumus luas belah ketupat: 0.5 * diagonal1 * diagonal2
-        return 0.5 * diagonal1 * diagonal2;
+        luas = 0.5 * diagonal1 * diagonal2;
+        return luas;
     }
 
     // Implementasi metode hitungKeliling() untuk belah ketupat
     @Override
     public double hitungKeliling() {
         // Rumus keliling belah ketupat: 4 × sisi miring
-        // Sisi miring dihitung dari √((d1/2)² + (d2/2)²)
-        double sisi = Math.sqrt(Math.pow((1.0/2.0 * diagonal1), 2) + Math.pow(1.0/2.0 * diagonal2, 2));
-        return 4 * sisi;
+        keliling = 4 * sisi;
+        return keliling;
+    }
+    
+    public double getLuas() {
+        return luas;
+    }
+    
+    public double getKeliling() {
+        return keliling;
     }
 }
